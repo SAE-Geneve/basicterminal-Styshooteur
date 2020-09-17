e908c39ecf3655f4f842d907b61cb58f9ea55dfc
Victor gonnet <vicy@live.Fr>
2020-09-17 12:06:34 +0200

    ca me plait

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <windows.h>
+#include <iostream>
+#include <stdio.h> 
+#include <stdlib.h>    
+#include <time.h>
+#include <string>
 
 int main()
 {
-	MessageBox(nullptr, "World!", "Hello", MB_ICONEXCLAMATION);
+	
+	 int crepe = 3;
+		std::string apple_count = std::to_string(crepe) + " apples ";
+	char value = apple_count[5];
+		std::cout << "you have " << apple_count;
+		std::cout << "size: " << apple_count.length();
+		std::cout << " the character is " <<  value;
+
+		std::cout << " \n\nEnter your first name: ";
+		std::string first_name;
+		std::cin >> first_name;
+		std::cout << first_name;
+		std::string name;
+		std::getline(std::cin, name);
+		std::cout << name;
+	    return 0;
 }
